4eae0d2a11b558e36277c9720b6ffb287057e6f5
Vibhuti Dabas <92392538+vibhutidabas@users.noreply.github.com>
2025-04-21 14:08:12 +0100

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -27,25 +27,3 @@ def demo():
     print("\n--Ending conversation--")
     print("\n Information collected about the user:\n")
     print(state.extracted_information)
-
-# def demo():
-#     """A simple demo script to show the conversation between a user and AI"""
-#     print("--Starting chat demo, please enter your request to get started.--")
-
-#     state = ConversationState()
-
-#     while True:
-#         user_message = input("Vibhu: ")
-#         state.messages.append(Message(text=user_message, sender=Sender.USER))
-
-#         state = chat_response(state)
-
-#         while len(state.new_messages) > 0:
-#             message = state.new_messages.pop(0)
-#             print(f"AI: {message.text}")
-#             state.messages.append(message)
-
-#         if state.finished:
-#             print("--Ending conversation--")
-#             print("Information collected about the user:", state.extracted_information)
-#             break
